95f9e956fd6ebfbdd7a41628daa1fb6d17b9e6fe
Todd Flanders <todd.a.flanders@accenture.com>
2020-01-22 12:49:59 -0500

    add gitignore, spike http get

--- app/hello.py
+++ app/hello.py
@@ -1,1 +1,13 @@
 print('hello people')
+
+url = 'https://github.com/PillarTechnology/phonebooth/releases/tag/latest'
+
+# url = 'https://github.com/tflander/python-tag-release-spike/releases/tag/latest'
+
+def downloadFile(URL=None):
+    import httplib2
+    h = httplib2.Http(".cache")
+    resp, content = h.request(URL, "GET")
+    return content
+
+print(downloadFile(url))
